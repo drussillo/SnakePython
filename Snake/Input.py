d36a1eb233f3a6cee0badd29aaebcb2a077add7b
BigDave1234 <daviderussillo7@gmail.com>
2024-02-13 23:15:29 +0100

    Finally: got the basic movement done!
    Apparently i just had to update the segments from toe to head instead of the opposite. So instead of incrementing the index I just had to decrement it in the for loop. Works fine now. Obviously there are still some things to change but the hardest part seems done!

--- Snake/Input.py
+++ Snake/Input.py
@@ -23,23 +23,12 @@ def handle_input():
                 g.direction = 'w'
 
     global frame_count
-    global frame_shift
-    
     frame_count += 1
     if frame_count == 25:
         frame_count = 0
-        
-
-        if frame_shift == 0:
-            g.setBodDir(0, g.direction)
-        else:
-            current_segment_x = g.snake_body[frame_shift][0]
-            current_segment_y = g.snake_body[frame_shift][1]
-            fwd_segment_direction = g.snake_body[frame_shift-1][2]
-            g.snake_body[frame_shift] = (current_segment_x, current_segment_y, fwd_segment_direction)
-        
-        frame_shift += 1
-    if frame_shift == len(g.snake_body):
-            frame_shift = 0
+        for i in range(len(g.snake_body)-1, 0, -1):
+            fwd_segment_direction = g.snake_body[i-1][2]
+            g.setBodDir(i, fwd_segment_direction)
+        g.setBodDir(0, g.direction)        
 
     return True
