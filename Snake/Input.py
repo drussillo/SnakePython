e900b405c339a5714c3995ab87723ecf7d5f73f7
BigDave1234 <daviderussillo7@gmail.com>
2024-02-13 22:40:56 +0100

    Progress: Snake movement works! Kinda...
    The snake can currently only turn once all segments have the same postions. Will try to fix soon...

--- Snake/Input.py
+++ Snake/Input.py
@@ -1,6 +1,10 @@
+from pickle import FRAME
+from tkinter import CURRENT
 import Global as g
 
 clock = g.pygame.time.Clock()
+frame_count = 0
+frame_shift = 0
 
 def handle_input():
     for event in g.pygame.event.get():
@@ -11,34 +15,31 @@ def handle_input():
                 return False
             elif event.key == g.pygame.K_w:
                 g.direction = 'n'
-                g.setBodDir(0, 'n')
             elif event.key == g.pygame.K_s:
                 g.direction = 's'
-                g.setBodDir(0, 's')
             elif event.key == g.pygame.K_d:
                 g.direction = 'e'
-                g.setBodDir(0, 'e')
             elif event.key == g.pygame.K_a:
                 g.direction = 'w'
-                g.setBodDir(0, 'w')
-     
 
-    # TODO every 25 frames, update segment direction to following segment's direction
-    # this code is not functional rn
+    global frame_count
+    global frame_shift
+    
     frame_count += 1
-    if frame_count % 25 == 0:
-        frame_intervall += 1
-        g.setBodDir(frame_intervall, g.direction)
+    if frame_count == 25:
+        frame_count = 0
+        
+
+        if frame_shift == 0:
+            g.setBodDir(0, g.direction)
+        else:
+            current_segment_x = g.snake_body[frame_shift][0]
+            current_segment_y = g.snake_body[frame_shift][1]
+            fwd_segment_direction = g.snake_body[frame_shift-1][2]
+            g.snake_body[frame_shift] = (current_segment_x, current_segment_y, fwd_segment_direction)
+        
+        frame_shift += 1
+    if frame_shift == len(g.snake_body):
+            frame_shift = 0
 
     return True
-
-
-'''
-    for i in range(1, len(g.snake_body)):
-        if g.direction == 'n' or g.direction == 's':
-            if g.snake_body[i][0] == g.snake_body[i-1][0]:
-                g.setBodDir(i, g.direction)
-        elif g.direction == 'e' or g.direction == 'w':
-            if g.snake_body[i][1] == g.snake_body[i-1][1]:
-                g.setBodDir(i, g.direction)
-'''
--- Snake/Main.py
+++ Snake/Main.py
@@ -5,13 +5,14 @@ import Snake
 g.pygame.init()
 g.SCREEN.fill((255, 255, 255))
 
+
 running = True
 while running:
     running = Input.handle_input()
-     
+    
     Snake.draw()
     
-    print(g.snake_body)
+    print(g.snake_body+["     Frame:"+str(Input.frame_count)])
 
     g.pygame.display.flip()
     g.clock.tick(120)
