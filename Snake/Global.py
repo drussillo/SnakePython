97db1603c63f0c8240612465ad7a9d7eabd91bac
drussillo <daverussillo7@gmail.com>
2025-10-16 20:18:27 -0500

    - Added todo for future work

--- Snake/Global.py
+++ Snake/Global.py
@@ -134,6 +134,7 @@ bgtileset_snow:tuple[pygame.surface.Surface, ...] = (
     pygame.transform.scale(get_sprite(bgtiles, 30, 60, 15, 15), (d_tile_size, d_tile_size)),
     pygame.transform.scale(get_sprite(bgtiles, 45, 60, 15, 15), (d_tile_size, d_tile_size))
 )
+# TODO: snow / frozen background + more backgrounds?
 bgtileset_cherryblossom:tuple[pygame.surface.Surface, ...] = (
     pygame.transform.scale(get_sprite(bgtiles, 00, 75, 15, 15), (d_tile_size, d_tile_size)),
     pygame.transform.scale(get_sprite(bgtiles, 15, 75, 15, 15), (d_tile_size, d_tile_size)),
