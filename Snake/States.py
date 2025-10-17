97996a1bca06beb8f8444061b32e215086af9a28
drussillo <daverussillo7@gmail.com>
2025-10-16 20:16:40 -0500

    - Changed basic mode to grass only

--- Snake/States.py
+++ Snake/States.py
@@ -38,7 +38,7 @@ def init_settings() -> None:
 def init_mode_basic() -> None:
     global initalized_state
     if initalized_state != g.Gamestate.MODE_BASIC:
-        g.current_bgtileset = g.bgtileset_cherryblossom
+        g.current_bgtileset = g.bgtileset_grass
         g.generate_random_background()
         g.clear_object_stack()
         new_head_x, new_head_y = g.randomize_spawn_pos()
