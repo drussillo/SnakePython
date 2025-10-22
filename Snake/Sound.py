74bff6ba741c9cd7816566ddb462209984cdce65
drussillo <daverussillo7@gmail.com>
2025-10-21 20:45:30 -0500

    Added relative paths - Now executables can be built

--- Snake/Sound.py
+++ Snake/Sound.py
@@ -25,15 +25,15 @@ def play(type:Type) -> None:
     if g.sfx:
         match type:
             case Type.GROW:
-                path = "audio/grow.wav"
+                path = g.resource_path("audio/grow.wav")
             case Type.DEATH:
-                path = "audio/death.wav"
+                path = g.resource_path("audio/death.wav")
             case Type.SPAWN:
-                path = "audio/spawn.wav"
+                path = g.resource_path("audio/spawn.wav")
             case Type.DAMAGE:
-                path = "audio/damage.wav"
+                path = g.resource_path("audio/damage.wav")
             case _:
-                path = "audio/error.wav"
+                path = g.resource_path("audio/error.wav")
 
         sound = g.pygame.mixer.Sound(path)
         sound.set_volume(0.2)
